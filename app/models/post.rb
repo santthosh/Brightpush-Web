d1fc2c960916aff65260edcecb4b48efcf32dcbc
Mehul <mehul@ubuntu.ubuntu-domain>
2012-05-06 01:07:21 +0530

    change post url to live

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveResource::Base
-  self.site = "http://localhost:3000" # AwsSdbProxy host + port
+  self.site = "http://newsstandalpha.com/" # AwsSdbProxy host + port
   self.prefix = "/com.apple.notifications/"     # use your SimpleDB domain enclosed in /s
 end
