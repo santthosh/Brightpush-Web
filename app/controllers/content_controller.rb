195106ada5fff3bb223683783f24bf52f61dc25c
Mehul <mehul@ubuntu.ubuntu-domain>
2012-05-30 01:53:53 +0530

    different domain as per current host

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -5,6 +5,45 @@ class ContentController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def index
+    #require 'aws/s3'
+    #include AWS::S3
+    #
+    #system("rake db:create")
+    #system("rake db:migrate")
+    #system("rake saas:bootstrap")
+    #
+    #AWS::S3::Base.establish_connection!(
+    #  :access_key_id     => 'AKIAIERRYQXDX7KCTHPQ',
+    #  :secret_access_key => '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'
+    # )
+    #
+    #Bucket.create('brightpush_development_ios_certificate_pkcs12')
+    #abort "hello"
+    #if request.host == 'brightpush.in'
+    #  Bucket.create('brightpush_application_icons')
+    #  Bucket.create('brightpush_development_ios_certificate_pkcs12')
+    #  Bucket.create('brightpush_production_ios_certificate_pkcs12')
+    #  Bucket.create('brightpush_ios_certificates_pem')
+    #  Bucket.create('brightpush_c2dm_token_txt')
+    #elsif request.host == 'brightpushalpha.in'
+    #  Bucket.create('alpha_brightpush_application_icons')
+    #  Bucket.create('alpha_brightpush_development_ios_certificate_pkcs12')
+    #  Bucket.create('alpha_brightpush_production_ios_certificate_pkcs12')
+    #  Bucket.create('alpha_brightpush_ios_certificates_pem')
+    #  Bucket.create('alpha_brightpush_c2dm_token_txt')
+    #elsif request.host == 'brightpushbeta.in'
+    #  Bucket.create('beta_brightpush_application_icons')
+    #  Bucket.create('beta_brightpush_development_ios_certificate_pkcs12')
+    #  Bucket.create('beta_brightpush_production_ios_certificate_pkcs12')
+    #  Bucket.create('beta_brightpush_ios_certificates_pem')
+    #  Bucket.create('beta_brightpush_c2dm_token_txt')
+    #else
+    #  Bucket.create('local_brightpush_application_icons')
+    #  Bucket.create('local_brightpush_development_ios_certificate_pkcs12')
+    #  Bucket.create('local_brightpush_production_ios_certificate_pkcs12')
+    #  Bucket.create('local_brightpush_ios_certificates_pem')
+    #  Bucket.create('local_brightpush_c2dm_token_txt')
+    #end
   end
   
   def subscriber_save
