98154418bb8c326fad72e8d736f6f45f2287942b
Santthosh Selvadurai <santthosh@gmail.com>
2012-06-06 12:22:34 -0700

    Adjusting the SSH keys to make it compatible with TC deploys

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,9 +1,9 @@
 set :branch, "develop"
-# This IP 50.112.125.184 is proxied to brightpushalpha.in
+# This IP 50.112.125.184 is CNAME'D to brightpushalpha.in
 role :web, "50.112.125.184"                          # Your HTTP server, Apache/etc
 role :app, "50.112.125.184"                          # This may be the same as your `Web` server
 role :db,  "50.112.125.184", :primary => true        # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 ssh_options[:user] = "ubuntu"
-ssh_options[:keys] = ["#{File.dirname(__FILE__)}/../../../ops/aws-keys/us-west-oregon/brightpush-web.pem"] 
+ssh_options[:keys] = ["/data/ops/aws-keys/us-west-oregon/brightpush-web.pem"] 
