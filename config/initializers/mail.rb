9bc4fba6ae77456abd887182b2c0241851c8e1e7
Santthosh <santthosh@appinmap.com>
2012-11-11 08:15:04 -0800

    Remove mail password

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,6 +3,6 @@ ActionMailer::Base.smtp_settings = {
   :port => 587,
   :domain => 'brightapps.in',
   :user_name => 'santthosh@brightapps.in',
-  :password => 'santt*123',
+  :password => '',
   :authentication => 'plain',
   :enable_starttls_auto => true }
